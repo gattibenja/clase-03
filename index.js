63c32daaf7c0e1920367e3c917112b25db0241ef
gattibenja <gattibenja@gmail.com>
2023-12-04 17:24:34 -0300

    add express

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => { 
     res.statusCode = 200; 
     res.setHeader('Content-Type', 'application/json'); 
-    res.end('{ "mensaje": "Bienvenido"}');
+    res.end('{ "mensaje": "hola"}');
  }); 
 
 
